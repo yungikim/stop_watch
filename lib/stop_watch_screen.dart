bb9ca0434e429e8e582978eacd339f7585529e22
yungikim <dosa777@gmail.com>
2023-02-02 22:44:53 +0900

    Init

---
+++ lib/stop_watch_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class StopWatchScreen extends StatefulWidget {
+  const StopWatchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StopWatchScreen> createState() => _StopWatchScreenState();
+}
+
+class _StopWatchScreenState extends State<StopWatchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('스톱워치'),
+      ),
+      body: Column(
+        children: [
+          SizedBox(height:30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: const [
+              Text(
+                '0',
+                style: TextStyle(fontSize: 50),
+              ),
+              Text('00'),
+            ],
+          ),
+          SizedBox(
+            width: 100,
+            height: 200,
+            child: ListView(
+              children: [
+                Center(child: Text('111')),
+                Text('111'),
+                Text('111'),
+                Text('111'),
+                Text('111'),
+                Text('111'),
+                Text('111'),
+                Text('111'),
+              ],
+            ),
+          ),
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                backgroundColor: Colors.orange,
+                onPressed: () {},
+                child: const Icon(Icons.refresh),
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.blue,
+                onPressed: () {},
+                child: const Icon(Icons.play_arrow),
+              ),
+              FloatingActionButton(
+                backgroundColor: Colors.green,
+                onPressed: () {},
+                child: const Icon(Icons.add),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          )
+        ],
+      ),
+    );
+  }
+}
